0047ba1b0d01ff2e1dd05531c7b70a44a783001d
Noémie <mallet.noemie46@gmail.com>
2024-11-15 19:08:50 +0100

    Correctifs mineurs x2

--- votes-ideas.php
+++ votes-ideas.php
@@ -81,7 +81,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                     if ($voteType === "positive") {
                         $a = $ideas[$index]['votes']['positive'] + 1;
+                        $b = $ideas[$index]['votes']['negative'];
                     } else if ($voteType === "negative") {
+                        $a = $ideas[$index]['votes']['positive'];
                         $b = $ideas[$index]['votes']['negative'] - 1;
                     }
                     echo $a . " = " . $b;
